b99f8e958a762fe2ace061a865b07871378e8891
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2025-03-19 14:20:35 +0700

    feat: update header and footer components to exclude rendering on admin and Aika chat pages

--- frontend/src/components/ui/FooterWrapper.tsx
+++ frontend/src/components/ui/FooterWrapper.tsx
@@ -6,8 +6,9 @@ import Footer from "./Footer";
 export default function FooterWrapper() {
   const pathname = usePathname();
   
-  // Don't render the footer on the Aika chat page
-  if (pathname?.startsWith('/aika')) {
+  // Don't render the footer on the Aika chat page or admin subpages
+  if (pathname?.startsWith('/aika') || 
+     (pathname?.startsWith('/admin/') && pathname !== '/admin')) {
     return null;
   }
   
--- frontend/src/components/ui/HeaderWrapper.tsx
+++ frontend/src/components/ui/HeaderWrapper.tsx
@@ -5,11 +5,10 @@ import Header from "./Header";
 
 export default function HeaderWrapper() {
   const pathname = usePathname();
-
-  const listOfPaths = ['/aika', '/admin/*'];
   
-  // Don't render the header on the listed pages
-  if (listOfPaths.some((path) => pathname?.startsWith(path))) {
+  // Don't render the header on admin and aika chat pages
+  if (pathname?.startsWith('/aika') || 
+     (pathname?.startsWith('/admin/') && pathname !== '/admin')) {
     return null;
   }
   
